67277f42467385cb26a04bf425c46274442ccf3e
Alyssa Winn <alyssawinn2@hotmail.com>
2021-03-03 17:15:20 -0700

    add js file

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+var pullCraigslistApi = function() {
+    debugger
+    var apiUrl = 'http://reference.craigslist.org/Categories/132';
+    fetch(apiUrl);
+    console.log(apiUrl);
+}
+
+pullCraigslistApi();
