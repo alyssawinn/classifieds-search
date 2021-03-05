85f60cad0e24ce48614586541af5292d453a2ef1
tristan <westenskowtristan122@gmail.com>
2021-03-04 19:33:32 -0700

    fixing merging conflicts

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,12 +8,14 @@ $("#btn").click(function () {
   console.log(inputValue);
 });
 
-let recentSearches = JSON.parse(localStorage.getItem("recentSearches")) ?? [];
-
-$("#btn").click(function () {
-  let inputValue = $("#search").val();
-  recentSearches.push(inputValue);
-  localStorage.setItem("recentSearches", JSON.stringify(recentSearches));
-  $("#search").val("");
-  console.log(inputValue);
+let ebayEl = document.querySelector("#ebayResults");
+let amznEl = document.querySelector("#amznResults");
+$(window).resize(function () {
+  if ($(window).width() < 810) {
+    ebayEl.classList.add("small-6");
+    amznEl.classList.add("small-6");
+  } else {
+    ebayEl.classList.remove("small-6");
+    amznEl.classList.remove("small-6");
+  }
 });
