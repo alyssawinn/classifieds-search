0c2a7002f725776317bec80737bd572a1ebd1a72
tristan <westenskowtristan122@gmail.com>
2021-03-04 18:07:30 -0700

    added value of the text input to local storage when button is clicked also cleared search bar on click

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+let recentSearches = JSON.parse(localStorage.getItem("recentSearches")) ?? [];
+
+$("#btn").click(function () {
+  let inputValue = $("#search").val();
+  recentSearches.push(inputValue);
+  localStorage.setItem("recentSearches", JSON.stringify(recentSearches));
+  $("#search").val("");
+  console.log(inputValue);
+});
