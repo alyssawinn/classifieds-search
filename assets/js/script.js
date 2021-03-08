b4b3fccffd3038a66c71fd26f73ab98f0b054a99
tristan <westenskowtristan122@gmail.com>
2021-03-08 13:03:40 -0700

    merging so i can get back to my feature branch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,14 @@
+var pullCraigslistApi = function () {
+  var apiUrl = "http://www.ksl.com/classifieds/api.php?cmd=ad&id=23027643";
+  fetch(apiUrl).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        console.log(data.type);
+      });
+    }
+  });
+};
+
 let recentSearches = JSON.parse(localStorage.getItem("recentSearches")) ?? [];
 
 $("#btn").click(function () {
